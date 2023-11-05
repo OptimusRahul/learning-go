3fe651ecdae43ed3a8dbb6b99ed569aa36673a1a
OptimusRahul <rahul@sparrowmarketing.com>
2023-11-05 12:29:35 +0530

    error handling in go

--- 2. create-a-go-module/hello/hello.go
+++ 2. create-a-go-module/hello/hello.go
@@ -3,10 +3,26 @@ package main
 import (
 	"fmt"
 	"greetings"
+	"log"
 )
 
 func main() {
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file and line number.
+	log.SetPrefix("greeting: ")
+	log.SetFlags(0)
+
+	// Request a greeting message
 	// Get a greeting message and print it.
-	message := greetings.Hello("Rahul")
+	message, err := greetings.Hello("")
+	// if an error was returne, print it to the console and
+	// exit the program
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// if no error was returned, print the returned message
+	// to the console.
 	fmt.Println(message)
 }
